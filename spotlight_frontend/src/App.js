42e18fbe38e49f0ccd9e2c508616fcab32f975e4
reececrowther <reececrowther@hotmail.co.uk>
2023-01-23 21:40:30 +0000

    App change

--- spotlight_frontend/src/App.js
+++ spotlight_frontend/src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route, useNavigate } from 'react-router-dom';
 import Login from './components/Login';
 import Home from './container/Home';
 
+
 function App() {
   return (
     <Routes>
