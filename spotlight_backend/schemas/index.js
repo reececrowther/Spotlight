769e23a80dbb25cc35133f6d97e583f08ae6918e
reececrowther <reececrowther@hotmail.co.uk>
2023-01-23 21:38:53 +0000

    first commit

---
+++ spotlight_backend/schemas/index.js
@@ -0,0 +1,7 @@
+import user from "./user";
+import comment from "./comment";
+import like from "./like";
+import photo from "./photo";
+import postedby from "./postedby";
+
+export const schemaTypes = [user, comment, like, photo, postedby]
---
+++ spotlight_backend/schemas/photo.js
@@ -0,0 +1,52 @@
+export default{
+    name: 'photo',
+    title: 'Photo',
+    type: 'document',
+    fields: [
+        {
+            name: 'title',
+            title: 'Title',
+            type: 'string'
+        },
+        {
+            name: 'about',
+            title: 'About',
+            type: 'string'
+        },
+        {
+            name: 'category',
+            title: 'Category',
+            type: 'string'
+        },
+        {
+            name: 'image',
+            title: 'Image',
+            type: 'image',
+            options: {
+                hotspot: true,
+            }
+        },
+        {
+            name: 'userId',
+            title: 'UserID',
+            type: 'string'
+        },
+        {
+            name: 'postedBy',
+            title: 'PostedBy',
+            type: 'postedBy'
+        },
+        {
+            name: 'like',
+            title: 'Like',
+            type: 'array',
+            of: [{type: 'like'}]
+        },
+        {
+            name: 'comments',
+            title: 'Comments',
+            type: 'array',
+            of: [{type: 'comment'}]
+        },
+    ]
+}
