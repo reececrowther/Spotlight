6d78543eb08e8e5be35e88b8b54d8e71cb30a8fa
reececrowther <reececrowther@hotmail.co.uk>
2023-01-24 21:21:24 +0000

    Login Screen

--- spotlight_frontend/src/components/Login.jsx
+++ spotlight_frontend/src/components/Login.jsx
@@ -1,8 +1,82 @@
-import React from 'react'
+import React from 'react';
+import '../css/login.css';
+import { GoogleLogin } from '@react-oauth/google';
+import { useNavigate } from 'react-router-dom';
+import jwt_decode from "jwt-decode";
+import { FcGoogle } from 'react-icons/fc';
+import  bgVideo  from '../assets/share.mp4';
+import { client } from '../client';
 
 function Login() {
+
+  const navigate = useNavigate();
+
+  const googleResponse = (response) => {
+    const token = response.credential;
+    var decoded = jwt_decode(token);
+    console.log(decoded);
+
+    localStorage.setItem('user', JSON.stringify(decoded));
+    
+    const googleId = response.clientId;
+    const { name, picture } = decoded;
+
+    const doc = {
+      _id: googleId,
+      _type: 'user', 
+      userName: name,
+      image: picture,
+    }
+
+    client.createIfNotExists(doc)
+    .then(() => {
+      navigate('/', { replace: true })
+    })
+
+  }
+
+
+
   return (
-    <div>Login</div>
+    <div className='login-section'>
+      <div className='login-con'>
+        <video 
+          src={bgVideo}
+          type="video/mp4"
+          loop
+          controls={false}
+          muted
+          autoPlay
+          className='bg-video'
+        />
+        <div className='login-form'>
+          <div className='logo'>
+            Spotlight Logo
+          </div>
+          <div className='google-signin'>
+            <GoogleLogin 
+              clientId={process.env.REACT_APP_GOOGLE_API_TOKEN} 
+              render={(renderProps) => (
+                <button
+                  type='button'
+                  className='google-button'
+                  onClick={renderProps.onClick}
+                  disabled={renderProps.disabled}
+                >
+                  <FcGoogle /> Sign in with Google
+                </button>
+              )}
+              onSuccess={googleResponse}
+              onFailure={googleResponse}
+              cookiePolicy="single_host_origin"
+              onError={() => {
+                console.log('Login Failed');
+              }}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- spotlight_frontend/src/index.js
+++ spotlight_frontend/src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
+    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_API_TOKEN} >
     <App />
+    </GoogleOAuthProvider>
   </Router>
 );
 
