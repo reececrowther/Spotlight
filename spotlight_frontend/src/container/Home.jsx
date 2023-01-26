20aef317cd0ed2acd2a77753cace33e0082e8664
reececrowther <reececrowther@hotmail.co.uk>
2023-01-26 20:55:56 +0000

    Sidebar Setup

--- spotlight_frontend/src/container/Home.jsx
+++ spotlight_frontend/src/container/Home.jsx
@@ -1,8 +1,52 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react';
+import { HiMenu } from 'react-icons/hi';
+import { AiFillCloseCircle } from 'react-icons/ai';
+import { Link, Route, Routes} from 'react-router-dom';
+import { Sidebar, UserProfile} from '../components';
+import { userQuery } from '../utils/data';
+import Photo from './Photos';
+import { client } from '../client';
 
 function Home() {
+
+  const [toggleSidebar, setToggleSidebar] = useState(false);
+  const [user, setUser] = useState(null);
+
+  const userInfo = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
+
+  useEffect(() => {
+    const query = userQuery(userInfo?.aud);
+
+    client.fetch(query).then((data) => {
+      setUser(data[0]);
+      
+    })
+  }, []);
+  
+
   return (
-    <div>Home</div>
+    <div className='home-con'>
+      <div className='sidebar'>
+        <Sidebar user={user && user} />
+      </div>
+      <div className='sidebar-mob'>
+        <HiMenu fontSize={40} className="hamburger" onClick={() => setToggleSidebar(true)}/>
+        <Link to="/">
+          Spotlight Logo
+        </Link>
+        <Link to={`user-profile/${user?._id}`}>
+          <img src={user?.image} alt="" />
+        </Link>
+      </div>
+      {toggleSidebar && (
+        <div className="sidebar-ani">
+          <div className="sidebar-close">
+            <AiFillCloseCircle fontSize={30} className="cursor-pointer" onClick={() => setToggleSidebar(false)} />
+          </div>
+          <Sidebar closeToggle={setToggleSidebar} user={user && user} />
+        </div>
+        )}
+    </div>
   )
 }
 
